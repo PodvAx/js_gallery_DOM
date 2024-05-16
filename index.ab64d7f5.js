cd2b60c9a345efd5042223a24b94a2543a78f593
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-05-16 16:10:32 +0300

    make build

---
+++ index.ab64d7f5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");if(r){t.preventDefault();var c=r.href;e.src=c}});
+//# sourceMappingURL=index.ab64d7f5.js.map
